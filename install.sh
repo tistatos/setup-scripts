50f44a523ec1bca13f239cea25918bed6b3e8ecf
Erik Sandrén <tistatos@gmail.com>
2020-05-09 17:56:50 +0200

    Reorganize install script
    prepare for questional logic when running the script

--- install.sh
+++ install.sh
@@ -36,43 +36,57 @@ sudo apt-get install -y \
 	xfce4-volumed \
 	zsh \
 
-#TODO: screensaver
-#TODO: sleep/hibernate mode
-#TODO: sleep mode
+# TODO: screensaver setup
+# TODO: sleep/hibernate mode on laptop lid
+# TODO: sleep mode on desktop
 
-#fix icons for thunar
-echo "gtk-icon-theme-name=\"gnome\"" >> ~/.gtkrc-2.0
-#install rust
+# oh-my-zsh install
+chsh -s/bin/zsh $USER
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# install rust
 curl https://sh.rustup.rs -sSf | sh
+# TODO: rustfmt and clippy
 
-#get dot files
-mkdir -p ~/.i3
-ln -s $PWD/dotfiles/i3/config ~/.i3/config
-
-ln -s $PWD/dotfiles/vim/.vimrc ~/.vimrc
-
-mkdir -p ~/.config/nvim
-ln -s $PWD/dotfiles/nvim/init.vim ~/.config/nvim
-
-ln -s $PWD/dotfiles/compton/compton.config ~/.config/compton.config
-#TODO: zsh
-#TODO: global git
-
-#ruby
+# ruby
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --rails
 
-#install vundle
+
+################
+# CONFIG FILES #
+################
+
+# TODO: fix the plugins for zsh
+# TODO: config files for zsh and oh my shell
+
+# fix icons for thunar
+echo "gtk-icon-theme-name=\"gnome\"" >> ~/.gtkrc-2.0
+
+# i3
+mkdir -p ~/.i3
+ln -s $PWD/dotfiles/i3/config ~/.i3/config
+# TODO: i3status files
+
+# vim
+ln -s $PWD/dotfiles/vim/.vimrc ~/.vimrc
+mkdir -p ~/.config/nvim
+ln -s $PWD/dotfiles/nvim/init.vim ~/.config/nvim
+
+# vim: install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-#vim install
+# vim: install python binding
 pip3 install setuptools
 pip3 install neovim
 pip install setuptools
 pip install neovim
-#ycm install
-#TODO
 
-#zsh install
-chsh -s/bin/zsh $USER
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# ycm install
+# TODO: add ycm and evaluate if ale is better?
+
+# compton
+ln -s $PWD/dotfiles/compton/compton.config ~/.config/compton.config
+
+# TODO: zsh config file
+# TODO: global config git
